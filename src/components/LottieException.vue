8ef4a02ed6507841defa00677f370bb280f00a1e
Robert Huang <56649783@qq.com>
2025-07-16 14:40:13 +0800

    refactor: 📦rename component

--- src/components/LottieException.vue
+++ src/components/LottieException.vue
@@ -2,13 +2,13 @@
 * @Author                : Robert Huang<56649783@qq.com>
 * @CreatedDate           : 2022-05-29 19:44:00
 * @LastEditors           : Robert Huang<56649783@qq.com>
-* @LastEditDate          : 2025-07-16 00:16:21
+* @LastEditDate          : 2025-07-16 14:31:18
 * @FilePath              : docs-web/src/components/ExceptionLottie.vue
 * @CopyRight             : Dedienne Aerospace China ZhuHai
 -->
 
 <template>
-  <LottiePlayer
+  <LottiePlayerVue
     :animationLink="'json/' + ErrorCode + '.json'"
     :height="400"
     :width="400"
@@ -17,7 +17,7 @@
 </template>
 
 <script setup>
-import LottiePlayer from 'src/components/LottiePlayer.vue'
+import LottiePlayerVue from 'src/components/LottiePlayer.vue'
 /* eslint-disable */
 const props = defineProps(['ErrorCode'])
 </script>
--- src/pages/ExceptionPage.vue
+++ src/pages/ExceptionPage.vue
@@ -2,17 +2,17 @@
 * @Author                : Robert Huang<56649783@qq.com>
 * @CreatedDate           : 2023-06-23 01:49:00
 * @LastEditors           : Robert Huang<56649783@qq.com>
-* @LastEditDate          : 2025-07-16 00:12:47
+* @LastEditDate          : 2025-07-16 14:32:55
 * @FilePath              : docs-web/src/pages/ExceptionPage.vue
 * @CopyRight             : Dedienne Aerospace China ZhuHai
 -->
 
 <template>
-  <q-page><ExceptionLottieVue :ErrorCode="ErrorCode" /></q-page>
+  <q-page><LottieExceptionVue :ErrorCode="ErrorCode" /></q-page>
 </template>
 
 <script setup>
-import ExceptionLottieVue from 'src/components/ExceptionLottie.vue'
+import LottieExceptionVue from 'src/components/LottieException.vue'
 
 /* eslint-disable */
 const props = defineProps(['ErrorCode'])
