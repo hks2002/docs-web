f2ab7db5e69deb75f780f17bc64ae18ab974f441
Robert Huang <56649783@qq.com>
2025-10-17 16:44:08 +0800

    feat: ✨reduce the lang pack

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -2,18 +2,31 @@
  * @Author                : Robert Huang<56649783@qq.com>                     *
  * @CreatedDate           : 2025-04-04 00:07:12                               *
  * @LastEditors           : Robert Huang<56649783@qq.com>                     *
- * @LastEditDate          : 2025-07-16 20:31:40                               *
+ * @LastEditDate          : 2025-10-17 16:39:51                               *
  * @FilePath              : docs-web/src/boot/i18n.js                         *
  * @CopyRight             : Dedienne Aerospace China ZhuHai                   *
  *****************************************************************************/
 import { storeToRefs } from 'pinia'
 import { Quasar } from 'quasar'
+import enUSQuasarLangPack from 'quasar/lang/en-US.js'
+import zhCNQuasarLangPack from 'quasar/lang/zh-CN.js'
 import { watch } from 'vue'
 import { createI18n } from 'vue-i18n'
 
 import { defineBoot } from '#q-app/wrappers'
 
+import enUSMessages from 'src/i18n/en-US.json'
+import zhCNMessages from 'src/i18n/zh-CN.json'
 import { useLocalStore } from 'src/stores/LocalStore'
+
+const quasarLangPacks = {
+  'zh-CN': zhCNQuasarLangPack,
+  'en-US': enUSQuasarLangPack,
+}
+const appMessages = {
+  'zh-CN': zhCNMessages,
+  'en-US': enUSMessages,
+}
 
 const i18n = createI18n({
   legacy: false,
@@ -41,16 +54,17 @@ export default defineBoot(({ app }) => {
   const loadLanguage = (langIso) => {
     console.debug('\u001b[35m' + '[i18n] ', langIso)
     // Set Quasar lang pack
-    import(`quasar/lang/${langIso}.js`).then((langPack) => {
-      Quasar.lang.set(langPack.default)
-    })
+    const langPack = quasarLangPacks[langIso]
+    if (langPack) {
+      Quasar.lang.set(langPack)
+    }
 
     // Set app i18n lang
-    import(`src/i18n/${langIso}.json`).then((messages) => {
-      // set locale and locale message
+    const messages = appMessages[langIso]
+    if (messages) {
       i18n.global.locale.value = langIso
-      i18n.global.setLocaleMessage(langIso, messages.default)
-    })
+      i18n.global.setLocaleMessage(langIso, messages)
+    }
   }
 
   const { locale } = storeToRefs(useLocalStore())
